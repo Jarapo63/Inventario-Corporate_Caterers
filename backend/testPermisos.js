cf76a8d8d327c0a646f7a86d9ac0ed561cc3f145
Manolo <mgarciam1903@gmail.com>
2026-04-27 13:55:17 -0600

    chore: limpieza de repositorio, mover scripts de prueba a carpeta ignorada

--- backend/testPermisos.js
+++
@@ -1,8 +0,0 @@
-const { sheets, SHEET_ID } = require('./db');
-
-async function check() {
-  const meta = await sheets.spreadsheets.values.get({ spreadsheetId: SHEET_ID, range: 'Permisos!A:D' });
-  console.log(meta.data.values);
-}
-
-check();
